ea7ef21b318f151cbfdcb81c3ef638270cc74bb9
Fabian Knorr <git@fabian-knorr.info>
2018-05-14 20:13:59 +0200

    Correctly handle week start, show currently paused work day

--- timeclock/timesheet.py
+++ timeclock/timesheet.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from datetime import timedelta
+from math import floor
 from os import path
 
 import appdirs
@@ -32,6 +33,8 @@ def collect(stamps):
 
     if last_transition.is_opening():
         day_intervals.append((last_opening, None))
+
+    if day_intervals:
         yield day_intervals
 
 
@@ -42,7 +45,13 @@ def time_table(days: list, now: Arrow):
         pause = ((end if end is not None else now) - begin) - work_time
         begin = begin.to('local')
         end = end.to('local').time() if end is not None else 'still working'
-        yield (begin.date(), begin.time(), pause, end, work_time)
+        yield (begin.date(), begin.time(), str(pause) + ' h', end, str(work_time) + ' h')
+
+
+def fmt_hours(hours: float):
+    h = floor(hours)
+    m = floor(60 * (hours - h))
+    return '{:02d}:{:02d}'.format(int(h), int(m))
 
 
 def main():
@@ -53,32 +62,41 @@ def main():
     args = parser.parse_args()
 
     cfg = config.load(args.config)
-    now = arrow.now()
+    now = arrow.utcnow()
 
     stamp_dir = path.expanduser(cfg['stamps']['dir'])
-    days = list(collect(map(Stamp.load, sorted(iter_stamps(stamp_dir)))))
+    stamps = [Stamp.load(s) for s in sorted(iter_stamps(stamp_dir))]
 
-    print(tabulate(time_table(days, now), headers=('date', 'begin', 'pause', 'end', 'time worked')))
+    days = list(collect(stamps))
+    table = tabulate(time_table(days, now), disable_numparse=True, stralign='right',
+                     headers=('date', 'begin', 'pause', 'end', 'time worked'))
+    if stamps[-1].transition == Transition.PAUSE:
+        table += ' (paused)'
+    print(table)
 
     work_time = timedelta()
 
     if days:
-        current_week = days[-1][0][0].floor('week')
+        current_week = now.floor('week')
         for day_intervals in reversed(days):
             if day_intervals[0][0].floor('week') != current_week:
                 break
 
-            work_time += sum(((e if e is not None else now) - b for b, e in day_intervals), timedelta())
+            work_time += sum(((e if e is not None else now) - b for b, e in day_intervals),
+                             timedelta())
 
     schedule = Schedule()
 
-    hours_required = timedelta(hours=schedule.hours_per_week)
-    print('\nWorked {} of {} required ({:.0f}%)'.format(work_time, hours_required, 100 * work_time / hours_required))
+    hours_worked = work_time.total_seconds() / 3600
+    hours_required = float(schedule.hours_per_week)
+    print('\nWorked {} of {} required ({:.0f}%).'.format(
+        fmt_hours(hours_worked), fmt_hours(hours_required), 100 * hours_worked / hours_required),
+        end=' ')
 
-    if work_time < hours_required:
-        print('{} to go.'.format(hours_required - work_time))
-    elif work_time > hours_required:
-        print('Made {} overtime.'.format(work_time - hours_required))
+    if hours_worked < hours_required:
+        print('{} to go this week.'.format(fmt_hours(hours_required - hours_worked)))
+    elif hours_worked > hours_required:
+        print('Made {} overtime this week.'.format(fmt_hours(work_time - hours_required)))
     else:
         print('Just on time!')
 
